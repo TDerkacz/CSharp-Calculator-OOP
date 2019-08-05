3c7e33ce8c806f342e48b26f1f581df738831fa0
teder <t.der@wp.pl>
2019-08-05 22:35:49 +0200

    Repository init

---
+++ CalculatorOOP/CalculatorOOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
