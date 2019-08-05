e5caee185294ec154679f4bcfc04efadec697dce
teder <t.der@wp.pl>
2019-08-05 23:08:11 +0200

    Defining basic operations

--- CalculatorOOP/CalculatorOOP/Program.cs
+++ CalculatorOOP/CalculatorOOP/Program.cs
@@ -6,7 +6,28 @@ namespace CalculatorOOP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int firstNumber = 10;
+            int secondNumber = 20;
+
+            int result;
+
+            // addition
+            result = firstNumber + secondNumber;
+            Console.WriteLine($"Adding numbers : {result}");
+
+            // subtraction
+            result = firstNumber - secondNumber;
+            Console.WriteLine($"Subtracting numbers : {result}");
+
+            // multiplication
+            result = firstNumber * secondNumber;
+            Console.WriteLine($"Multiplying numbers : {result}");
+
+            // division
+            result = firstNumber / secondNumber;
+            Console.WriteLine($"Dividing numbers : {result}");
+
+            Console.ReadKey();
         }
     }
 }
