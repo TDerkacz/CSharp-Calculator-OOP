8b5d1f7774a662a4e70c74289137fb1e31aae8c9
teder <t.der@wp.pl>
2019-08-08 23:36:08 +0200

    Methods that holds operations moved to Calculator class, methods and class are public

---
+++ CalculatorOOP/CalculatorOOP/Calculator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorOOP
+{
+    public class Calculator
+    {
+        public static decimal? AddNumbers(string firstNumberFromConsole, string secondNumberFromConsole)
+        {
+            decimal firstNumber, secondNumber;
+            decimal? result = null;
+
+            if ((Decimal.TryParse(firstNumberFromConsole, out firstNumber)) && (Decimal.TryParse(secondNumberFromConsole, out secondNumber)))
+            {
+                result = firstNumber + secondNumber;
+            }
+            else
+            {
+                Console.WriteLine("Incorrect data, both must be numbers");
+            }
+            return result;
+        }
+
+        public static decimal? SubtractNumbers(string firstNumberFromConsole, string secondNumberFromConsole)
+        {
+            decimal firstNumber, secondNumber;
+            decimal? result = null;
+
+            if ((Decimal.TryParse(firstNumberFromConsole, out firstNumber)) && (Decimal.TryParse(secondNumberFromConsole, out secondNumber)))
+            {
+                result = firstNumber - secondNumber;
+            }
+            else
+            {
+                Console.WriteLine("Incorrect data, both must be numbers");
+            }
+            return result;
+        }
+
+        public static decimal? MultiplyNumbers(string firstNumberFromConsole, string secondNumberFromConsole)
+        {
+            decimal firstNumber, secondNumber;
+            decimal? result = null;
+
+            if ((Decimal.TryParse(firstNumberFromConsole, out firstNumber)) && (Decimal.TryParse(secondNumberFromConsole, out secondNumber)))
+            {
+                result = firstNumber * secondNumber;
+            }
+            else
+            {
+                Console.WriteLine("Incorrect data, both must be numbers");
+            }
+            return result;
+        }
+
+        public static decimal? DivideNumbers(string firstNumberFromConsole, string secondNumberFromConsole)
+        {
+            decimal firstNumber, secondNumber;
+            decimal? result = null;
+
+            if ((Decimal.TryParse(firstNumberFromConsole, out firstNumber)) && (Decimal.TryParse(secondNumberFromConsole, out secondNumber)))
+            {
+                if (secondNumber != 0)
+                {
+                    result = firstNumber / secondNumber;
+                }
+                else
+                {
+                    Console.WriteLine("Cannot divide by zero!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Incorrect data, both must be numbers");
+            }
+            return result;
+        }
+    }
+}
--- CalculatorOOP/CalculatorOOP/Program.cs
+++ CalculatorOOP/CalculatorOOP/Program.cs
@@ -17,16 +17,16 @@ namespace CalculatorOOP
             Console.WriteLine("Enter second number");
             var secondNumberFromConsole = Console.ReadLine();
 
-            result = AddNumbers(firstNumberFromConsole, secondNumberFromConsole);
+            result = Calculator.AddNumbers(firstNumberFromConsole, secondNumberFromConsole);
             PrintResult(result, "Adding numbers");
 
-            result = SubtractNumbers(firstNumberFromConsole, secondNumberFromConsole);
+            result = Calculator.SubtractNumbers(firstNumberFromConsole, secondNumberFromConsole);
             PrintResult(result, "Subtracting numbers");
 
-            result = MultiplyNumbers(firstNumberFromConsole, secondNumberFromConsole);
+            result = Calculator.MultiplyNumbers(firstNumberFromConsole, secondNumberFromConsole);
             PrintResult(result, "Multiplying numbers");
 
-            result = DivideNumbers(firstNumberFromConsole, secondNumberFromConsole);
+            result = Calculator.DivideNumbers(firstNumberFromConsole, secondNumberFromConsole);
             PrintResult(result, "Dividing numbers");
 
             Console.WriteLine();
@@ -44,77 +44,6 @@ namespace CalculatorOOP
             {
                 Console.WriteLine($"{ResultInfo} : {result}");
             }
-        }
-
-        static decimal? AddNumbers(string firstNumberFromConsole, string secondNumberFromConsole)
-        {
-            decimal firstNumber, secondNumber;
-            decimal? result = null;
-
-            if ((Decimal.TryParse(firstNumberFromConsole, out firstNumber)) && (Decimal.TryParse(secondNumberFromConsole, out secondNumber)))
-            {
-                result = firstNumber + secondNumber;
-            }
-            else
-            {
-                Console.WriteLine("Incorrect data, both must be numbers");
-            }
-            return result;
-        }
-
-        static decimal? SubtractNumbers(string firstNumberFromConsole, string secondNumberFromConsole)
-        {
-            decimal firstNumber, secondNumber;
-            decimal? result = null;
-
-            if ((Decimal.TryParse(firstNumberFromConsole, out firstNumber)) && (Decimal.TryParse(secondNumberFromConsole, out secondNumber)))
-            {             
-                result = firstNumber - secondNumber;
-            }
-            else
-            {
-                Console.WriteLine("Incorrect data, both must be numbers");
-            }
-            return result;
-        }
-
-        static decimal? MultiplyNumbers(string firstNumberFromConsole, string secondNumberFromConsole)
-        {
-            decimal firstNumber, secondNumber;
-            decimal? result = null;
-
-            if ((Decimal.TryParse(firstNumberFromConsole, out firstNumber)) && (Decimal.TryParse(secondNumberFromConsole, out secondNumber)))
-            {
-                result = firstNumber * secondNumber;
-            }
-            else
-            {
-                Console.WriteLine("Incorrect data, both must be numbers");
-            }
-            return result;
-        }
-
-        static decimal? DivideNumbers(string firstNumberFromConsole, string secondNumberFromConsole)
-        {
-            decimal firstNumber, secondNumber;
-            decimal? result = null;
-
-            if ((Decimal.TryParse(firstNumberFromConsole, out firstNumber)) && (Decimal.TryParse(secondNumberFromConsole, out secondNumber)))
-            {        
-                if (secondNumber != 0)
-                {
-                    result = firstNumber / secondNumber;
-                }
-                else
-                {
-                    Console.WriteLine("Cannot divide by zero!");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Incorrect data, both must be numbers");
-            }
-            return result;
         }
     }
 }
