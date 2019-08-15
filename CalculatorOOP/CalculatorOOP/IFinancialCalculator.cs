dc0f5ddccde703ad1f6b6bd901c22604a4e815cf
teder <t.der@wp.pl>
2019-08-16 00:42:07 +0200

    IFinancialCalculator interface added

---
+++ CalculatorOOP/CalculatorOOP/IFinancialCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorOOP
+{/// <summary>
+/// Interface 
+/// no field declarations
+/// no methods implementations 
+/// only methods declarations
+/// 
+/// Interface can be used as a parameter in method
+/// so method will know from witch class to use
+/// 
+/// Abstract Class might contain
+/// - private or protected variables
+/// </summary>
+/// 
+    public interface IFinancialCalculator
+    {
+        void CalculateAnnuallyFinances();
+        void CalculateMonthlyFinances();
+        void CalculateWeeklyFinances();
+    }
+}
