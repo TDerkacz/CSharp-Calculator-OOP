927258e727c86422656f196280acdd83681e5813
teder <t.der@wp.pl>
2019-08-16 00:52:26 +0200

    Classes are using IFinancialCalculator

--- CalculatorOOP/CalculatorOOP/Classes/CarCompanyFinancialCalculator.cs
+++ CalculatorOOP/CalculatorOOP/Classes/CarCompanyFinancialCalculator.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace CalculatorOOP.Classes
 {
-    public class CarCompanyFinancialCalculator
+    public class CarCompanyFinancialCalculator : IFinancialCalculator
     {
+        public void CalculateAnnuallyFinances()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CalculateMonthlyFinances()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CalculateWeeklyFinances()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- CalculatorOOP/CalculatorOOP/Classes/TreeCompanyFinancialCalculator.cs
+++ CalculatorOOP/CalculatorOOP/Classes/TreeCompanyFinancialCalculator.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace CalculatorOOP.Classes
 {
-    public class TreeCompanyFinancialCalculator
+    public class TreeCompanyFinancialCalculator : IFinancialCalculator
     {
+        public void CalculateAnnuallyFinances()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CalculateMonthlyFinances()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CalculateWeeklyFinances()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
