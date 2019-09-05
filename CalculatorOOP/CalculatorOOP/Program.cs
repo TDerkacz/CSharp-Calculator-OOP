f77949f0c96b47c1b213fc738be85064a5321ec5
teder <t.der@wp.pl>
2019-09-05 22:08:52 +0200

    two calculations methods - called with same name but operating on two objects

--- CalculatorOOP/CalculatorOOP/Program.cs
+++ CalculatorOOP/CalculatorOOP/Program.cs
@@ -28,9 +28,42 @@ namespace CalculatorOOP
             Console.WriteLine($"TESCO monthly results = {monthlyResults}");
             Console.WriteLine($"TESCO weekly results = {weeklyResults}");
 
-            Console.ReadKey();
 
+            Console.WriteLine();
+            Console.WriteLine(">> two calculations methods - called with same name but operating on two objects ");
+            Console.WriteLine();
+
+            CalculateFinances(obiFinancialCalc, 20000, 1500, 50);
+            Console.WriteLine();
+            CalculateFinances(tescoFinancialCalc, 20000, 1500, 50);
+
+            Console.ReadKey();
         }
+
+        
+
+        static void CalculateFinances(ObiFinancialCalc obiFinancialCalc, decimal annualInput, decimal monthlyInput, decimal weeklyInput)
+        {
+            decimal annualResults = obiFinancialCalc.CalculateAnnualFinances(annualInput);
+            decimal monthlyResults = obiFinancialCalc.CalculateMonthlyFinances(monthlyInput);
+            decimal weeklyResults = obiFinancialCalc.CalculateWeeklyFinances(weeklyInput);
+
+            Console.WriteLine($"OBI annual results = {annualResults}");
+            Console.WriteLine($"OBI monthly results = {monthlyResults}");
+            Console.WriteLine($"OBI weekly results = {weeklyResults}");
+        }
+
+        static void CalculateFinances(TescoFinancialCalc tescoFinancialCalc, decimal annualInput, decimal monthlyInput, decimal weeklyInput)
+        {
+            decimal annualResults = tescoFinancialCalc.CalculateAnnualFinances(annualInput);
+            decimal monthlyResults = tescoFinancialCalc.CalculateMonthlyFinances(monthlyInput);
+            decimal weeklyResults = tescoFinancialCalc.CalculateWeeklyFinances(weeklyInput);
+
+            Console.WriteLine($"TESCO annual results = {annualResults}");
+            Console.WriteLine($"TESCO monthly results = {monthlyResults}");
+            Console.WriteLine($"TESCO weekly results = {weeklyResults}");
+        }
+
 
         static void PrintResult(decimal? result, string ResultInfo)
         {
@@ -45,30 +78,3 @@ namespace CalculatorOOP
         }
     }
 }
-
-//decimal? result = null;
-
-//Console.WriteLine("==== This is simple Calculator with four operations ====");
-//            Console.WriteLine();
-
-//            Console.WriteLine("Enter first number");
-//            var firstNumberFromConsole = Console.ReadLine();
-
-//Console.WriteLine("Enter second number");
-//            var secondNumberFromConsole = Console.ReadLine();
-
-//result = Calculator.AddNumbers(firstNumberFromConsole, secondNumberFromConsole);
-//            PrintResult(result, "Adding numbers");
-
-//result = Calculator.SubtractNumbers(firstNumberFromConsole, secondNumberFromConsole);
-//            PrintResult(result, "Subtracting numbers");
-
-//result = Calculator.MultiplyNumbers(firstNumberFromConsole, secondNumberFromConsole);
-//            PrintResult(result, "Multiplying numbers");
-
-//result = Calculator.DivideNumbers(firstNumberFromConsole, secondNumberFromConsole);
-//            PrintResult(result, "Dividing numbers");
-
-//Console.WriteLine();
-//            Console.WriteLine("Press any key to exit");
-//            Console.ReadKey();
